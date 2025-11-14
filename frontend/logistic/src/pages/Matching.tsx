97e5b6e43a0c84f73127acda251da21e92d2170f
Mateusz Jedrzejczak <251536@edu.p.lodz.pl>
2025-11-14 17:46:17 +0100

    fix

--- frontend/logistic/src/pages/Matching.tsx
+++ frontend/logistic/src/pages/Matching.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
-import { Sparkles, Eye, EyeOff, User, Package } from 'lucide-react'
+import { Sparkles, Eye, EyeOff, User, Package, Truck } from 'lucide-react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { Navbar } from '@/components/layout'
 
