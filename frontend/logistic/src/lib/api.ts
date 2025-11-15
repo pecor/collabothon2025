74717e24714de452680b256d521c0939b2b1e130
Kacper Kleczaj <kleczaj.kacper@gmail.com>
2025-11-15 14:27:12 +0100

    fix api

--- frontend/logistic/src/lib/api.ts
+++ frontend/logistic/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://backend-truckai-project.apps.cluster-bt99s.bt99s.sandbox1766.opentlc.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
